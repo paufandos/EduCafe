75461e60eb4bcd7fdd016a13dadea7992b493236
ElenaMon <emonleongarcera@gmail.com>
2023-01-19 13:48:11 +0100

    fix(#52) burbuja carrito

--- src/web/js/carrito.js
+++ src/web/js/carrito.js
@@ -75,25 +75,24 @@ class Carrito {
 				if (!dialog.open) {
 					dialog.showModal();
 				}
-				
-		}else{
+			});
+
+		} else {
 			dialog.close();
 		}
 	}
 
-	numeroArticulosTotal(){
-		let numProductos=0;
-		
+	numeroArticulosTotal() {
+		let numProductos = 0;
+
 		this.productos.forEach(element => {
-			numProductos+=element.unidades;
+			numProductos += element.unidades;
 		});
 		let burbuja = document.getElementById("cart_menu_num");
-		burbuja.innerHTML=numProductos;
+		burbuja.innerHTML = numProductos;
 		burbuja.classList = "c-icon__burbuja c-icon--xsmall";
 	}
-	
-	
-}
+
 
 
 	async getProducts() {
