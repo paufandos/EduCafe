3a5de980a460163cfe8d2bfa5bea0680a89c8182
javierdelmoralcamacho@gmail.com <javierdelmoralcamacho@gmail.com>
2023-02-02 13:15:17 +0100

    Migraciones

---
+++ educafe-laravel/database/migrations/2023_02_02_105508_categorias.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id();//se escribe sin nada porque es autoincremental
+            $table->string('nombre');
+            $table->timestamps(); //crea el registro de creacion
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorias');
+    }
+};
